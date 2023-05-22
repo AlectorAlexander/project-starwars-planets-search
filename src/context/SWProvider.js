61711e966e82d091e9b2d2aa843b055a2d917675
Aléctor Alexander <alector.alexandre@gmail.com>
2023-05-22 18:14:02 -0300

    fix: dependências atualizdas

--- src/context/SWProvider.js
+++ src/context/SWProvider.js
@@ -1,66 +1,66 @@
 /* eslint-disable no-await-in-loop */
-import PropTypes from 'prop-types';
-import React, { useState } from 'react';
-import fetchStarWars from '../api/API';
-import SWContext from './SWContext';
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import fetchStarWars from "../api/API";
+import SWContext from "./SWContext";
 
 function Provider({ children }) {
-  const [data, setData] = useState('');
-  const [table, setTable] = useState('');
-  const [loading, setLoad] = useState(true);
-  const [filterByName, setFilter] = useState({});
-  const [search, setSearch] = useState('');
-  const [column, setColumn] = useState(['population', 'orbital_period', 'diameter',
-    'rotation_period', 'surface_water']);
-  const [filterByNumeric, setFilterByNumeric] = useState({ filterByNumericValues: [] });
+    const [data, setData] = useState("");
+    const [table, setTable] = useState("");
+    const [loading, setLoad] = useState(true);
+    const [filterByName, setFilter] = useState({});
+    const [search, setSearch] = useState("");
+    const [column, setColumn] = useState(["population", "orbital_period", "diameter",
+        "rotation_period", "surface_water"]);
+    const [filterByNumeric, setFilterByNumeric] = useState({ filterByNumericValues: [] });
 
-  async function setStarWarsState() {
-    try {
-      const API = await fetchStarWars();
-      const filmsFetch = async (urlFilms) => Promise.all(urlFilms.map(async (url) => {
-        const response = await fetch(url);
-        const titleFetch = await response.json();
-        return `"${titleFetch.title}" `;
-      }));
-      for (let i = 0; i < API.results.length; i += 1) {
-        const FilmsTitle = [];
-        const p = await filmsFetch(API.results[i].films);
-        FilmsTitle.push(p);
-        API.results[i].films = FilmsTitle;
-      }
-      setData(API.results);
-      setTable(API.results);
-      setLoad(false);
-    } catch (err) {
-      console.error(err);
+    async function setStarWarsState() {
+        try {
+            const API = await fetchStarWars();
+            const filmsFetch = async (urlFilms) => Promise.all(urlFilms.map(async (url) => {
+                const response = await fetch(url);
+                const titleFetch = await response.json();
+                return `"${titleFetch.title}" `;
+            }));
+            for (let i = 0; i < API.results.length; i += 1) {
+                const FilmsTitle = [];
+                const p = await filmsFetch(API.results[i].films);
+                FilmsTitle.push(p);
+                API.results[i].films = FilmsTitle;
+            }
+            setData(API.results);
+            setTable(API.results);
+            setLoad(false);
+        } catch (err) {
+            console.error(err);
+        }
     }
-  }
-  const contextValue = {
-    loading,
-    data,
-    search,
-    table,
-    filterByName,
-    filterByNumeric,
-    column,
-    setColumn,
-    setFilterByNumeric,
-    setFilter,
-    setData,
-    setTable,
-    setStarWarsState,
-    setSearch,
-  };
+    const contextValue = {
+        loading,
+        data,
+        search,
+        table,
+        filterByName,
+        filterByNumeric,
+        column,
+        setColumn,
+        setFilterByNumeric,
+        setFilter,
+        setData,
+        setTable,
+        setStarWarsState,
+        setSearch,
+    };
 
-  return (
-    <SWContext.Provider value={ contextValue }>
-      {children}
-    </SWContext.Provider>
-  );
+    return (
+        <SWContext.Provider value={ contextValue }>
+            {children}
+        </SWContext.Provider>
+    );
 }
 
 Provider.propTypes = {
-  children: PropTypes.node.isRequired,
+    children: PropTypes.node.isRequired,
 };
 
 export default Provider;
--- src/hooks/Filters.js
+++ src/hooks/Filters.js
@@ -1,74 +1,73 @@
 /* eslint-disable max-len */
-/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-case-declarations */
 /* eslint-disable array-callback-return */
-import React, { useContext, useEffect } from 'react';
-import '../styles/Filters.css';
-import { Button } from 'react-bootstrap';
-import { RiDeleteBin2Fill } from 'react-icons/ri';
-import SWContext from '../context/SWContext';
+import React, { useContext, useEffect } from "react";
+import "../styles/Filters.css";
+import { Button } from "react-bootstrap";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+import SWContext from "../context/SWContext";
 
 function Filters() {
-  const { filterByNumeric, setFilterByNumeric,
-    column: coluna, setColumn, setTable, table, data } = useContext(SWContext);
+    const { filterByNumeric, setFilterByNumeric,
+        column: coluna, setColumn, setTable, table, data } = useContext(SWContext);
 
-  useEffect(() => {
+    useEffect(() => {
+        const { filterByNumericValues } = filterByNumeric;
+        filterByNumericValues.map((elem) => {
+            const { column, comparison, value } = elem;
+            switch (comparison) {
+            case "maior que":
+                setTable(table
+                    .filter((element) => Number(element[column]) > Number(value)));
+                break;
+            case "menor que":
+                setTable(table
+                    .filter((element) => Number(element[column]) < Number(value)));
+                break;
+            case "igual a":
+                setTable(table
+                    .filter((element) => Number(element[column]) === Number(value)));
+                break;
+
+            default:
+                break;
+            }
+        });
+    }, [filterByNumeric]);
+
+    const deleteFromTheFilter = (itemList) => {
+        const { filterByNumericValues } = filterByNumeric;
+        const newFilter = filterByNumericValues
+            .filter((element) => element.column !== itemList);
+        setFilterByNumeric({ filterByNumericValues: newFilter });
+        setColumn([...coluna, itemList]);
+        setTable(data);
+    };
+
     const { filterByNumericValues } = filterByNumeric;
-    filterByNumericValues.map((elem) => {
-      const { column, comparison, value } = elem;
-      switch (comparison) {
-      case 'maior que':
-        setTable(table
-          .filter((element) => Number(element[column]) > Number(value)));
-        break;
-      case 'menor que':
-        setTable(table
-          .filter((element) => Number(element[column]) < Number(value)));
-        break;
-      case 'igual a':
-        setTable(table
-          .filter((element) => Number(element[column]) === Number(value)));
-        break;
-
-      default:
-        break;
-      }
-    });
-  }, [filterByNumeric]);
-
-  const deleteFromTheFilter = (itemList) => {
-    const { filterByNumericValues } = filterByNumeric;
-    const newFilter = filterByNumericValues
-      .filter((element) => element.column !== itemList);
-    setFilterByNumeric({ filterByNumericValues: newFilter });
-    setColumn([...coluna, itemList]);
-    setTable(data);
-  };
-
-  const { filterByNumericValues } = filterByNumeric;
-  return (
-    <ul className="d-flex justify-content-center flex-column flex text-white h-100 filters">
-      { filterByNumericValues
+    return (
+        <ul className="d-flex justify-content-center flex-column flex text-white h-100 filters">
+            { filterByNumericValues
       && filterByNumericValues.map((element, i) => {
-        const { column, comparison, value } = element;
-        return (
-          <li
-            key={ i }
-          >
-            {` ${column} ${comparison} ${value} `}
-            <Button
-              variant="warning"
-              onClick={ () => deleteFromTheFilter(column) }
-              className="buttonVolts"
-              type="button"
-            >
-              <RiDeleteBin2Fill className="iconVolts" />
-            </Button>
-          </li>
-        );
+          const { column, comparison, value } = element;
+          return (
+              <li
+                  key={ i }
+              >
+                  {` ${column} ${comparison} ${value} `}
+                  <Button
+                      variant="warning"
+                      onClick={ () => deleteFromTheFilter(column) }
+                      className="buttonVolts"
+                      type="button"
+                  >
+                      <RiDeleteBin2Fill className="iconVolts" />
+                  </Button>
+              </li>
+          );
       })}
-    </ul>
-  );
+        </ul>
+    );
 }
 
 export default Filters;
